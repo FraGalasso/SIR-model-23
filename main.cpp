3841be06befc00a4df1cd4e30191b776e2b71d46
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2023-05-01 11:31:12 +0200

    Little optimization

--- main.cpp
+++ main.cpp
@@ -7,17 +7,18 @@
 #include "sir.hpp"
 
 void print_SIR(SIR m) {
-  std::cout << "Susceptibles: " << m.get_s() << "\nInfected: " << m.get_i()
+  std::cout << "Susceptibles: " << m.get_s() << "\nInfectious: " << m.get_i()
             << "\nRemoved: " << m.get_r() << '\n';
 }
 
 int main() {
-  Parameters p{0.7, 0.1};
-  SIR model{1000, 20};
+  Parameters p{0.3, 0.015};
+  SIR model{100000, 20};
+  SIR previous{};
 
-  for (int j = 0; j < 500; ++j) {
-    const int N{model.get_s() + model.get_i() + model.get_r()};
+  const int N{model.get_s() + model.get_i() + model.get_r()};
 
+  for (int j = 0; j < 50000; ++j) {
     // ottengo i valori di s, i, r in floating point
     double s1{model.get_s() - p.beta * model.get_s() * model.get_i() / N};
     double i1{model.get_i() + p.beta * model.get_s() * model.get_i() / N -
@@ -55,14 +56,24 @@ int main() {
     }
     // a questo punto i dovrebbe essere uguale a = 0
     if ((s + i + r) != N) {
-      throw std::runtime_error("oh oh\n");
+      throw std::runtime_error(
+          "Something went wrong with rounding operations.\n");
     }
 
-    std::cout << s << '\t' << i << '\t' << r << '\t' << s + i + r << '\n';
+    previous.set_s(model.get_s());
+    previous.set_i(model.get_i());
+    previous.set_r(model.get_r());
 
     model.set_s(s);
     model.set_i(i);
     model.set_r(r);
+
+    // se parto sempre dagli stessi valori interi otterrò sempre gli stessi
+    // risultati
+    if (model == previous) {
+      std::cout << "Converged after " << j << " steps.\nBreaking loop.\n";
+      break;
+    }
   }
   print_SIR(model);
 }
