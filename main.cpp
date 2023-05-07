5e8fe3a926a118486de33aeefd60c6cd6da31bde
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2023-05-07 19:11:19 +0200

    Minimal error handling.

--- main.cpp
+++ main.cpp
@@ -40,84 +40,89 @@ int insert_people() {
 }
 
 int main() {
-  std::cout << "Input beta.\n";
-  double beta{insert_parameter()};
-  std::cout << "Input gamma.\n";
-  double gamma{insert_parameter()};
-  Parameters p{beta, gamma};
-  SIR model{};
-  std::cout << "Input susceptibles.\n";
-  model.set_s(insert_people());
-  std::cout << "Input infectious.\n";
-  model.set_i(insert_people());
-  std::cout << "Input removed.\n";
-  model.set_r(insert_people());
-  SIR previous{};
+  try {
+    std::cout << "Input beta.\n";
+    double beta{insert_parameter()};
+    std::cout << "Input gamma.\n";
+    double gamma{insert_parameter()};
+    Parameters p{beta, gamma};
+    SIR model{};
+    std::cout << "Input susceptibles.\n";
+    model.set_s(insert_people());
+    std::cout << "Input infectious.\n";
+    model.set_i(insert_people());
+    std::cout << "Input removed.\n";
+    model.set_r(insert_people());
+    SIR previous{};
 
-  const int N{model.get_s() + model.get_i() + model.get_r()};
+    const int N{model.get_s() + model.get_i() + model.get_r()};
 
-  for (int j = 0; j < 50000; ++j) {
-    // computing floating-point values for s, i, r
-    double s1{model.get_s() - p.beta * model.get_s() * model.get_i() / N};
-    double i1{model.get_i() + p.beta * model.get_s() * model.get_i() / N -
-              p.gamma * model.get_i()};
-    double r1{model.get_r() + p.gamma * model.get_i()};
+    for (int j = 0; j < 50000; ++j) {
+      // computing floating-point values for s, i, r
+      double s1{model.get_s() - p.beta * model.get_s() * model.get_i() / N};
+      double i1{model.get_i() + p.beta * model.get_s() * model.get_i() / N -
+                p.gamma * model.get_i()};
+      double r1{model.get_r() + p.gamma * model.get_i()};
 
-    // computing integer part
-    int s{static_cast<int>(s1)};
-    int i{static_cast<int>(i1)};
-    int r{static_cast<int>(r1)};
+      // computing integer part
+      int s{static_cast<int>(s1)};
+      int i{static_cast<int>(i1)};
+      int r{static_cast<int>(r1)};
 
-    // separating decimal part
-    s1 -= s;
-    i1 -= i;
-    r1 -= r;
+      // separating decimal part
+      s1 -= s;
+      i1 -= i;
+      r1 -= r;
 
-    // equivalent to sum of decimal parts
-    int diff{N - (s + i + r)};
+      // equivalent to sum of decimal parts
+      int diff{N - (s + i + r)};
 
-    // distributing remainder from decimal parts, one step at a time
-    while (diff > 0) {
-      if (s1 >= i1 && s1 >= r1) {
-        ++s;
-        s1 = 0;
-        --diff;
-      } else if (i1 >= s1 && i1 >= r1) {
-        ++i;
-        i1 = 0;
-        --diff;
-      } else {
-        ++r;
-        r1 = 0;
-        --diff;
+      // distributing remainder from decimal parts, one step at a time
+      while (diff > 0) {
+        if (s1 >= i1 && s1 >= r1) {
+          ++s;
+          s1 = 0;
+          --diff;
+        } else if (i1 >= s1 && i1 >= r1) {
+          ++i;
+          i1 = 0;
+          --diff;
+        } else {
+          ++r;
+          r1 = 0;
+          --diff;
+        }
+      }
+      // at this point diff should be equal to 0
+
+      previous.set_s(model.get_s());
+      previous.set_i(model.get_i());
+      previous.set_r(model.get_r());
+
+      model.set_s(s);
+      model.set_i(i);
+      model.set_r(r);
+
+      // no point in going on if I'm always starting from the same integers
+      if (model == previous) {
+        std::cout << "Converged after " << j << " steps.\nBreaking loop.\n";
+        break;
       }
     }
-    // at this point diff should be equal to 0
+    print_SIR(model);
 
-    previous.set_s(model.get_s());
-    previous.set_i(model.get_i());
-    previous.set_r(model.get_r());
-
-    model.set_s(s);
-    model.set_i(i);
-    model.set_r(r);
-
-    // no point in going on if I'm always starting from the same integers
-    if (model == previous) {
-      std::cout << "Converged after " << j << " steps.\nBreaking loop.\n";
-      break;
+    std::ofstream out_file("results.txt");
+    if (!out_file) {
+      std::cerr << "Failed opening results.txt.\n";
+      return EXIT_FAILURE;
     }
-  }
-  print_SIR(model);
-
-  std::ofstream out_file("results.txt");
-  if (!out_file) {
-    std::cerr << "Failed opening results.txt.\n";
+    out_file << std::setw(15) << "Susceptibles:" << std::setw(15)
+             << "Infectious:" << std::setw(15) << "Removed:" << '\n'
+             << std::setw(15) << model.get_s() << std::setw(15) << model.get_i()
+             << std::setw(15) << model.get_r() << '\n';
+    out_file.close();
+  } catch (std::exception const& e) {
+    std::cerr << "Caught exception: '" << e.what() << "'\n";
     return EXIT_FAILURE;
   }
-  out_file << std::setw(15) << "Susceptibles:" << std::setw(15)
-           << "Infectious:" << std::setw(15) << "Removed:" << '\n'
-           << std::setw(15) << model.get_s() << std::setw(15) << model.get_i()
-           << std::setw(15) << model.get_r() << '\n';
-  out_file.close();
 }
