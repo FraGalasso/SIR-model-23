e903466a135718c94e26c2d14161505f9d829dbe
itsLollo21 <106199565+itsLollo21@users.noreply.github.com>
2023-06-27 18:25:56 +0200

    Update persona.hpp

--- persona.hpp
+++ persona.hpp
@@ -1,9 +1,9 @@
 #ifndef PERSONA_HPP
 #define PERSONA_HPP
 
+#include <iostream>
 #include <random>
 #include <vector>
-#include <iostream>
 
 enum class Stato { Susceptible, Infectious, Removed };
 
@@ -103,93 +103,39 @@ class Popolazione {
   }
 
   void collision() {
-    for (int i = 0; i < size(); ++i) {
-      v[i] = GetPerson(i);
-      v[i + 1] = GetPerson(i + 1);
-      int r = v[i].GetX();
-      int px = v[i].GetPx();
-      int py = v[i].GetPy();
-      int r_ = v[i + 1].GetX();
-      int px_ = v[i + 1].GetPx();
-      int py_ = v[i + 1].GetPy();
-      if (r == r_) {
-        px = -px;
-        px_ = -px_;
-        py = -py;
-        py_ = -py; 
-
-        /* if (px < 0) {
-           --r;
-           ++r_;
-         }
-         if (px > 0) {
-           ++r;
-           --r_;
-         }
-         if (py > 0) {
-           r += 10;
-           r_ -= 10;
-         }
-         if (py < 0) {
-           r -= 10;
-           r_ += 10;
-         }
-      }; */
-      }
-      
-        if (px == 1) {
-          if ((r % 10) == 9) {
-            r -= 9;
-          } else {
-            ++r;
-          }
-        }
-        if (px == -1) {
-          if (r % 10 == 0) {
-            r += 9;
-          } else {
-            --r;
-          }
-        }
-        if (py == 1) {
-          if (r >= 0 && r <= 9) {
-            r += 90;
-          } else {
-            r -= 10;
-          }
-        }
-        if (py == -1) {
-          if (r >= 90 && r <= 99) {
-            r -= 90;
-          } else {
-            r += 10;
-          }
-        }
-        if ((px * px) != 1 && (py * py) != 1) {
-          std::cout<<"Errore"<<'\n'; //da sistemare
-        }
-        
-        v[i].SetX(r);
-        v[i+1].SetX(r_);
-        v[i].SetPx(px);
+    for(int i = 0; i< size(); ++i){
+    int j= 1;
+      while (j+i < size()) {
+          v[i] = GetPerson(i);
+        v[i+j] = GetPerson(i + j);
+        int r = v[i].GetX();
+        int px = v[i].GetPx();
+        int py = v[i].GetPy();
+        int r_ = v[i + j].GetX();
+        int px_ = v[i + j].GetPx();
+        int py_ = v[i + j].GetPy();
+        if (r == r_) {
+          px = -px;
+          px_ = -px_;
+          py = -py;
+          py_ = -py;
+         v[i].SetPx(px);
         v[i].SetPy(py);
-        v[i + 1].SetPx(px_);
-        v[i + 1].SetPy(py_);
-        
-       /* if(r != r_){
-        v[i].SetX(r);
-        v[i+1].SetX(r_);
-        } */
-       /* v[i].SetPx(px);
-        v[i].SetPy(py);
-        v[i + 1].SetPx(px_);
-        v[i + 1].SetPy(py_);
-        //v[i + 1].SetX(r_);
-        */
-        
+        v[i + j].SetPx(px_);
+        v[i + j].SetPy(py_);
+        }
+       ++j;
       }
-    };
+  }
   };
+  /* void infection(){
+     for (int i=0; i< size() ; ++i){
+     v[i] = GetPerson(i);
+     int r = v[i].GetX();
+     v[i];
 
+     }
+   } */
+};
 
 #endif
