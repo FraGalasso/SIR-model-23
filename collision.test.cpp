315984b6baf83b7a22926baf41df927fdfdbeea8
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2023-06-29 15:20:46 +0200

    Improved testing

--- collision.test.cpp
+++ collision.test.cpp
@@ -2,7 +2,7 @@
 #include "doctest.h"
 #include "persona.hpp"
 
-TEST_CASE("Testing Evolution with one element") {
+TEST_CASE("Testing collisions") {
   Stato S{};
   Persona p(1, -1, 0, S);
   Persona q(9, 1, 0, S);
@@ -11,16 +11,18 @@ TEST_CASE("Testing Evolution with one element") {
   V = {p, d, q};
   Popolazione pop{V};
   pop.evolve();
-  pop.collision();
   pop.evolve();
- 
+
   Persona p1 = pop.GetPerson(0);
   Persona d1 = pop.GetPerson(1);
   Persona q1 = pop.GetPerson(2);
   CHECK(p1.GetX() == 1);
-  CHECK(p1.GetPx()==1);
+  CHECK(p1.GetPx() == 1);
+  CHECK(p1.GetPy() == 0);
   CHECK(d1.GetX() == 5);
-  CHECK(d1.GetPx()==-1);
+  CHECK(d1.GetPx() == -1);
+  CHECK(d1.GetPy() == 1);
   CHECK(q1.GetX() == 9);
-  CHECK(q1.GetPx()==-1);
+  CHECK(q1.GetPx() == -1);
+  CHECK(q1.GetPy() == 0);
 };
