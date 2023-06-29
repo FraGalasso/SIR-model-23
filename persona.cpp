1c1d7c459e952ead14de392f3af72d90a52451a5
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2023-06-29 12:26:05 +0200

    Created persona.cpp

---
+++ persona.cpp
@@ -0,0 +1,138 @@
+#include "persona.hpp"
+
+void Popolazione::evolve() {
+  for (int i = 0; i < size(); ++i) {
+    v[i] = GetPerson(i);
+    int r = v[i].GetX();
+    int px = v[i].GetPx();
+    int py = v[i].GetPy();
+
+    if (v[i].GetStatus() != Stato::r) {
+      if (px == 1) {
+        if ((r % 10) == 9) {
+          // dx = -9;
+          r -= 9;
+        } else {
+          // dx = 1;
+          ++r;
+        }
+      }
+      if (px == -1) {
+        if (r % 10 == 0) {
+          // dx = 9;
+          r += 9;
+        } else {
+          --r;
+        }
+      }
+      if (py == 1) {
+        if (r >= 0 && r <= 9) {
+          // dy = 90;
+          r += 90;
+        } else {
+          // dy = -10;
+          r -= 10;
+        }
+      }
+      if (py == -1) {
+        if (r >= 90 && r <= 99) {
+          // dy = -90;
+          r -= 90;
+        } else {
+          // dy = 10;
+          r += 10;
+        }
+      }
+      if ((px * px) != 1 && (py * py) != 1) {
+        // dx = 0;
+        // dy = 0;
+        r = 0;
+      }
+      v[i].SetX(r);
+    }
+    if (v[i].GetStatus() == Stato::r) {
+      v[i].SetX(100);
+    }
+    infection();
+    collision();
+  }
+}
+
+void Popolazione::random_distribution() {
+  std::mt19937 gen(time(0));
+  // random bit generator (mersenne twister)
+  for (int i = 0; i < size(); ++i) {
+    std::uniform_int_distribution<> x_distribution(0, 99);
+    int x = x_distribution(gen);
+    std::uniform_int_distribution<> px_distribution(-1, 1);
+    int px = px_distribution(gen);
+    std::uniform_int_distribution<> py_distribution(-1, 1);
+    int py = py_distribution(gen);
+    v[i].SetX(x);
+    v[i].SetPx(px);
+    v[i].SetPy(py);
+  }
+}
+
+void Popolazione::status_distribution(int n) {
+  std::mt19937 gen(time(0));
+  // random bit generator (mersenne twister)
+  for (int i = 0; i < n; ++i) {
+    v[i].SetStatus(Stato::i);
+  }
+  for (int i = n; i < size(); ++i) {
+    v[i].SetStatus(Stato::s);
+  }
+}
+
+void Popolazione::collision() {
+  for (int i = 0; i < size(); ++i) {
+    int j = 1;
+    while (j + i < size()) {
+      v[i] = GetPerson(i);
+      v[i + j] = GetPerson(i + j);
+      int r = v[i].GetX();
+      int px = v[i].GetPx();
+      int py = v[i].GetPy();
+      int r_ = v[i + j].GetX();
+      int px_ = v[i + j].GetPx();
+      int py_ = v[i + j].GetPy();
+      if (r == r_) {
+        px = -px;
+        px_ = -px_;
+        py = -py;
+        py_ = -py;
+        v[i].SetPx(px);
+        v[i].SetPy(py);
+        v[i + j].SetPx(px_);
+        v[i + j].SetPy(py_);
+      }
+      ++j;
+    }
+  }
+}
+
+void Popolazione::infection() {
+  for (int i = 0; i < size(); ++i) {
+    int j = 1;
+    while (j + i < size()) {
+      v[i] = GetPerson(i);
+      v[i + j] = GetPerson(i + j);
+      int r = v[i].GetX();
+      int r_ = v[i + j].GetX();
+      Stato st1 = v[i].GetStatus();
+      Stato st2 = v[i + j].GetStatus();
+      if (r == r_) {
+        if (st1 == Stato::i && st2 == Stato::s) {
+          st2 = Stato::i;
+          v[i + j].SetStatus(st2);
+        }
+        if (st1 == Stato::s && st2 == Stato::i) {
+          st1 = Stato::i;
+          v[i].SetStatus(st1);
+        }
+      }
+      ++j;
+    }
+  }
+};
