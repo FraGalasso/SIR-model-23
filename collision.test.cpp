e91b36207df79978fd14ea94884a6f4c75df172b
itsLollo21 <106199565+itsLollo21@users.noreply.github.com>
2023-06-30 12:25:56 +0200

    Update collision.test.cpp

--- collision.test.cpp
+++ collision.test.cpp
@@ -2,76 +2,33 @@
 #include "doctest.h"
 #include "persona.hpp"
 
-TEST_CASE("Testing collisions") {
-  Persona p(1, -1, 0, Stato::s);
-  Persona q(9, 1, 0, Stato::s);
-  Persona d(27, -1, 1, Stato::s);
+TEST_CASE("Testing Evolution with one element") {
+  Stato S{};
+  
+  Persona p(1, -1, 0, S, 0);
+  Persona q(9, 1, 0, S, 0);
+  Persona d(10, 0, 1, S, 0);
+  Persona c(90,0,-1,S,0);
   std::vector<Persona> V;
-  V = {p, d, q};
+  V = {p, d, q, c};
   Popolazione pop{V};
   pop.evolve();
+ // pop.collision();
   pop.evolve();
 
-  Persona p1 = pop.GetPerson(0);
-  Persona d1 = pop.GetPerson(1);
-  Persona q1 = pop.GetPerson(2);
-  CHECK(p1.GetX() == 1);
-  CHECK(p1.GetPx() == 1);
-  CHECK(p1.GetPy() == 0);
-  CHECK(d1.GetX() == 5);
-  CHECK(d1.GetPx() == -1);
-  CHECK(d1.GetPy() == 1);
-  CHECK(q1.GetX() == 9);
-  CHECK(q1.GetPx() == -1);
-  CHECK(q1.GetPy() == 0);
-};
-
-// horrible fail
-TEST_CASE("Testing triple collisions") {
-  Persona p(1, -1, 0, Stato::s);
-  Persona q(9, 1, 0, Stato::s);
-  Persona d(11, -1, 1, Stato::s);
-  Popolazione pop{{p, d, q}};
-  pop.evolve();
-  pop.evolve();
-
-  Persona p1 = pop.GetPerson(0);
-  Persona d1 = pop.GetPerson(1);
-  Persona q1 = pop.GetPerson(2);
-  CHECK(p1.GetX() == 1);
-  CHECK(p1.GetPx() == 1);
-  CHECK(p1.GetPy() == 0);
-  CHECK(d1.GetX() == 11);
-  CHECK(d1.GetPx() == 1);
-  CHECK(d1.GetPy() == -1);
-  CHECK(q1.GetX() == 9);
-  CHECK(q1.GetPx() == -1);
-  CHECK(q1.GetPy() == 0);
-};
-
-TEST_CASE("Testing quadruple collisions") {
-  Persona p(1, -1, 0, Stato::s);
-  Persona q(9, 1, 0, Stato::s);
-  Persona d(11, -1, 1, Stato::s);
-  Persona c(90, 0, -1, Stato::s);
-  Popolazione pop{{p, d, q, c}};
-  pop.evolve();
-  pop.evolve();
-
+ 
   Persona p1 = pop.GetPerson(0);
   Persona d1 = pop.GetPerson(1);
   Persona q1 = pop.GetPerson(2);
   Persona c1 = pop.GetPerson(3);
   CHECK(p1.GetX() == 1);
-  CHECK(p1.GetPx() == 1);
-  CHECK(p1.GetPy() == 0);
-  CHECK(d1.GetX() == 11);
-  CHECK(d1.GetPx() == 1);
-  CHECK(d1.GetPy() == -1);
+  CHECK(p1.GetPx()==1);
+  CHECK(d1.GetX() == 10);
+  CHECK(d1.GetPx()==0);
+  CHECK(d1.GetPy()== -1);
   CHECK(q1.GetX() == 9);
-  CHECK(q1.GetPx() == -1);
-  CHECK(q1.GetPy() == 0);
-  CHECK(c1.GetX() == 90);
-  CHECK(c1.GetPx() == 0);
-  CHECK(c1.GetPy() == 1);
+  CHECK(q1.GetPx()==-1);
+  CHECK(c1.GetX()==90);
+  CHECK(c1.GetPy()==1);
+  CHECK(q1.GetCollision()==0);
 };
