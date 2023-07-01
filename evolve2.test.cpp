c78a9d49e03ba807e042481a583134f3cf9391bb
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2023-07-01 20:12:05 +0200

    Fixing little bug from merge

--- evolve2.test.cpp
+++ evolve2.test.cpp
@@ -52,27 +52,4 @@ TEST_CASE("Testing Evolution twice") {
   CHECK(b1.GetX() == 70);
   CHECK(b1.GetPx() == 1);
   CHECK(b1.GetPy() == 1);
-  ;
-};
-
-TEST_CASE("Testing Evolution twice") {
-  Persona p(29, 0, 1, Stato::s);
-  Persona q(99, 1, -1, Stato::s);
-  Persona b(98, 1, 1, Stato::s);
-  Popolazione pop{{p, q, b}};
-  pop.evolve();
-  pop.evolve();
-  Persona p1 = pop.GetPerson(0);
-  Persona q1 = pop.GetPerson(1);
-  Persona b1 = pop.GetPerson(2);
-  CHECK(p1.GetX() == 9);
-  CHECK(p1.GetPx() == 0);
-  CHECK(p1.GetPy() == 1);
-  CHECK(q1.GetX() == 11);
-  CHECK(q1.GetPx() == 1);
-  CHECK(q1.GetPy() == -1);
-  CHECK(b1.GetX() == 70);
-  CHECK(b1.GetPx() == 1);
-  CHECK(b1.GetPy() == 1);
-  ;
 };
